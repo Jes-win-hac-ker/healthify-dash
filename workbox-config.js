1523d74a577b3b02c5f053516299413c86050e20
Jes-win-hac-ker <Jes-win-hac-ker@users.noreply.github.com>
2025-11-09 00:16:48 +0530

    ci: add Workbox injectManifest step to build workflow for PWA precaching

---
+++ workbox-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  swSrc: 'public/service-worker.js',
+  swDest: 'dist/service-worker.js',
+  globDirectory: 'dist',
+  globPatterns: [
+    '**/*.{js,css,html,svg,png,ico,json}',
+  ],
+  // Don't precache the service worker itself
+  globIgnores: [
+    'service-worker.js',
+  ],
+  // Additional files to precache (like manifest.json, offline.html)
+  additionalManifestEntries: [
+    { url: '/manifest.json', revision: null },
+    { url: '/offline.html', revision: null },
+  ],
+};
